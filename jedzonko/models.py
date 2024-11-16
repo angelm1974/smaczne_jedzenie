042b17976ff35f1febce2ac296dacb527018075e
angelm1974 <mariusz980@gmail.com>
2024-11-16 17:24:32 +0100

    djan2024a

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -1,42 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Producent(models.Model):
     nazwa = models.CharField(max_length=64)
     kraj = models.CharField(max_length=64)
-    
+
     class Meta:
         verbose_name = 'Producent'
         verbose_name_plural = 'Producenci'
-    
+
     def __str__(self):
         return self.nazwa
-    
+
+
 class Surowiec(models.Model):
     nazwa = models.CharField(max_length=64)
     kategoria = models.Choices('nabiał', 'mięso', 'warzywa', 'owoce', )
-    cena= models.DecimalField(max_digits=5, decimal_places=2)
+    cena = models.DecimalField(max_digits=5, decimal_places=2)
     jednostka = models.CharField(max_length=64)
-    producent= models.ForeignKey(Producent, on_delete=models.CASCADE)
-    
+    producent = models.ForeignKey(Producent, on_delete=models.CASCADE)
+
     class Meta:
         verbose_name = 'Surowiec'
         verbose_name_plural = 'Surowce'
-    
+
     def __str__(self):
-        return self.nazwa  
+        return self.nazwa
+
 
 class Produkt(models.Model):
-    nazwa=models.CharField(max_length=64)
-    kategoria = models.Choices('napoje', 'słodycze',  'posiłki', )
+    wybor = [('napoje', 'napoje'),('słodycze', 'słodycze'),
+             ('posiłki', 'posiłki'), ('owoce', 'owoce'), ]
+    nazwa = models.CharField(max_length=64)
+    kategoria = models.CharField(max_length=64, choices=wybor, default='posiłki')
     cena = models.DecimalField(max_digits=5, decimal_places=2)
     opis = models.TextField()
     producent = models.ForeignKey(Producent, on_delete=models.CASCADE)
-    surowce= models.ManyToManyField(Surowiec)
-    
+    surowce = models.ManyToManyField(Surowiec)
+
     class Meta:
         verbose_name = 'Produkt'
         verbose_name_plural = 'Produkty'
-    
+
     def __str__(self):
         return self.nazwa
